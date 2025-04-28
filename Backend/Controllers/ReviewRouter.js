76d4322cefdd0acb6a278591e4fb0fb0e29355db
keerthana290506 <keerthana290506@gmail.com>
2025-04-28 09:06:11 +0530

    Bruno files updated

--- Backend/Controllers/ReviewRouter.js
+++ Backend/Controllers/ReviewRouter.js
@@ -27,7 +27,7 @@ reviewRouter.post("/review",async(req,res)=>{
 
 reviewRouter.get("/",async(req,res)=>{
   try{
-    const review = new Review.find()
+    const review = await Review.find()
     res.json.status(201).json(review)
   }catch(err){
     res.status(500).json({error:err.message})
